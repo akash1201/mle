7e20625942bb820101571e27a324fd72740006f7
Akash Chhetri <akash@aresourcepool.com>
2022-03-03 21:11:35 +0530

    commit

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/mlefrontend/build')))
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'mlefrontend', 'build', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {

027cf2ca85156cd81c180da12f62df68a46385b0
Akash Chhetri <akash@aresourcepool.com>
2022-03-03 21:06:49 +0530

    commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import userRoutes from './Routers/user.js';
 import incomeRoutes from './Routers/income.js';
 import morgan from 'morgan';
 import razorpayRoutes from './Routers/razorpay.js'
+import path from 'path'
 
 env.config()
 
@@ -25,6 +26,21 @@ mongoose.connect(process.env.DB_HOST).then(()=>{
 app.use(`/api/users`, userRoutes);
 app.use(`/api/income`, incomeRoutes);
 app.use(`/api/razorpay`, razorpayRoutes);
+
+const __dirname = path.resolve()
+// app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/mlefrontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 
 app.listen(3001, console.log('Server started'))

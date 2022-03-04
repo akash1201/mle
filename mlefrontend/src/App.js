12148add5fe65f04ff6cc95e3945cbe3dc4b87b8
Akash Chhetri <akash@aresourcepool.com>
2022-03-04 10:16:28 +0530

    commit

--- mlefrontend/src/App.js
+++ mlefrontend/src/App.js
@@ -1,10 +1,11 @@
 import React,{ useEffect } from 'react';
 import 'antd/dist/antd.css';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Routes,
+// } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import LoginScreen from './screens/LoginScreen';
 import GenerateRpin from './screens/GenerateRpin';
 import MyDashboard from './screens/MyDashboard';
@@ -25,11 +26,11 @@ function App() {
   },[])
   return (
     <Router basename='/'>
-                    <Routes>
-                      <Route path='/' element={<Home/>}>
-                      <Route index element={<Home/>}/>
-                      <Route path='login' element={<LoginScreen/>}/>
-                      <Route path='generate-rpin'element={<GenerateRpin/>}/>
+                    {/* <Routes> */}
+                      <Route path='/' component={Home}/>
+                      <Route index component={Home}/>
+                      <Route path='/login' component={LoginScreen}/>
+                      {/* <Route path='generate-rpin'element={<GenerateRpin/>}/>
                       <Route path='my-dashboard' element={<MyDashboard/>}/>
                       <Route path='add-downline' element={<AddDownline/>}/>
                       <Route path='membership-benefits' element={<CashBack />} />
@@ -37,9 +38,8 @@ function App() {
                       <Router path='add-members' element={<AddMember/>}/>
                       <Route path='add-vendor' element={<AddVendor />}/>
                       <Route path='generate-bill' element={<GenerateBill />}/>
-                      <Route path='users' element={<UserManagement />}/>
-                      </Route>
-                    </Routes>
+                      <Route path='users' element={<UserManagement />}/> */}
+                    {/* </Routes> */}
     </Router>
   );
 }

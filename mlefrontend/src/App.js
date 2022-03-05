f57c530612efe32c7e5cefb717c46bf3a87d99a4
Akash Chhetri <akash@aresourcepool.com>
2022-03-05 10:13:44 +0530

    commimt

--- mlefrontend/src/App.js
+++ mlefrontend/src/App.js
@@ -1,10 +1,5 @@
 import React,{ useEffect } from 'react';
 import 'antd/dist/antd.css';
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Routes,
-// } from "react-router-dom";
 import LoginScreen from './screens/LoginScreen';
 import GenerateRpin from './screens/GenerateRpin';
 import MyDashboard from './screens/MyDashboard';
@@ -16,6 +11,7 @@ import AddVendor from './screens/AddVendor';
 import GenerateBill from './VendorScreens/GenerateBill';
 import AddMember from './AdminScreens/AddMembers';
 import UserManagement from './screens/UserManagement';
+import { Routes, Route } from 'react-router-dom'
 
 
 
@@ -24,25 +20,20 @@ function App() {
     console.log('Here');
   },[])
   return (
-    <div>
-       <h1>Hii Welcome to JLE Mega Mart</h1>
-    </div>
-    // <Router>
-    //                 <Routes>
-    //                   <Route path='/' element={<Home />}/>
-    //                   {/* <Route index component={Home}/> */}
-    //                   <Route path='/login' element={<LoginScreen />}/>
-    //                   <Route path='generate-rpin'element={<GenerateRpin/>}/>
-    //                   <Route path='my-dashboard' element={<MyDashboard/>}/>
-    //                   <Route path='add-downline' element={<AddDownline/>}/>
-    //                   <Route path='membership-benefits' element={<CashBack />} />
-    //                   <Route path='vendor-management' element={<VendorManagement />} />
-    //                   <Router path='add-members' element={<AddMember/>}/>
-    //                   <Route path='add-vendor' element={<AddVendor />}/>
-    //                   <Route path='generate-bill' element={<GenerateBill />}/>
-    //                   <Route path='users' element={<UserManagement />}/>
-    //                 </Routes>
-    // </Router>
+                    <Routes>
+                      <Route path='/' element={<Home />}/>
+                      {/* <Route index component={Home}/> */}
+                      <Route path='login' element={<LoginScreen />}/>
+                      <Route path='generate-rpin'element={<GenerateRpin/>}/>
+                      <Route path='my-dashboard' element={<MyDashboard/>}/>
+                      <Route path='add-downline' element={<AddDownline/>}/>
+                      <Route path='membership-benefits' element={<CashBack />} />
+                      <Route path='vendor-management' element={<VendorManagement />} />
+                      <Route path='add-members' element={<AddMember/>}/>
+                      <Route path='add-vendor' element={<AddVendor />}/>
+                      <Route path='generate-bill' element={<GenerateBill />}/>
+                      <Route path='users' element={<UserManagement />}/>
+                    </Routes>
   );
 }
 
